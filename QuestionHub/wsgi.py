2fff25db86fc5a67c32114e852595b74b8797916
Saloni Sinha <saloni.sinha1425@gmail.com>
2017-12-01 20:08:00 +0530

    Modified some files

--- QuestionHub/wsgi.py
+++ QuestionHub/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Question_Paper_Generator.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "QuestionHub.settings")
 
 application = get_wsgi_application()
